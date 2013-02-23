de0795558758907a13ef4e8fe16637aaa125330d
Jacek Laskowski <jacek@japila.pl>
2013-02-23 02:07:56 +0100

    SMS receiving and reading content works

--- src/pl/japila/textmenbuy/SmsReceiver.java
+++ src/pl/japila/textmenbuy/SmsReceiver.java
@@ -4,17 +4,29 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
     public static final String SMS_EXTRA_NAME = "pdus";
+    private static final String TAG = "SmsReceiver";
 
+    @Override
     public void onReceive(Context context, Intent intent) {
+        Log.v(TAG, "Entering onReceive");
         Bundle extras = intent.getExtras();
-        String messages = "";
         if (extras != null) {
-            Object[] smsExtra = (Object[]) extras.get(SMS_EXTRA_NAME);
+            Object[] messages = (Object[]) extras.get(SMS_EXTRA_NAME);
+            SmsMessage smsMessage[] = new SmsMessage[messages.length];
+            for (int n = 0; n < messages.length; n++) {
+                smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+            }
+
+            // show first message
+            Toast toast = Toast.makeText(context, "Received SMS: " + smsMessage[0].getMessageBody(), Toast.LENGTH_LONG);
+            toast.show();
         }
-        Toast.makeText(context, "onReceive", Toast.LENGTH_SHORT).show();
+        Log.v(TAG, "Leaving onReceive");
     }
 }
